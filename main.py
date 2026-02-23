da811db88cf661cb1a30bd367aaae0a3478577d8
Jahongir <ojahongir2197@gmail.com>
2026-02-23 15:54:51 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+# 1
+def salom_ber(ism):
+    print(f"Salom, {ism}!")
+
+# 2
+def raqam_chiqar(son):
+    print(son)
+
+# 3
+def qoshish(a, b):
+    print(a + b)
+
+# 4
+def ism_yosh(ism, yosh):
+    print(f"{ism} {yosh} yoshda.")
+
+# 5. Kvadrat chiqarish
+def kvadrat(son):
+    print(son ** 2)
+
+# 6
+def matn_takrorla(matn, soni):
+    print(matn * soni)
+
+# 7
+def ayirish(a, b):
+    print(a - b)
+
+# 8
+def hurmatli(ism):
+    print(f"Hurmatli {ism}!")
+
+# 9
+def kopaytirish(a, b):
+    print(a * b)
+
+# 10
+def bolish(a, b):
+    print(a / b)
